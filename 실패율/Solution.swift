d87b86d29d1cd29f4a3dac1fd6adc09962e27601
Jaewon-Yun <81426024+woin2ee@users.noreply.github.com>
2022-10-03 19:20:08 +0900

    Update Solution.swift

--- 실패율/Solution.swift
+++ 실패율/Solution.swift
@@ -1,20 +1,13 @@
 import Foundation
 
-// N: 전체 스테이지의 개수
-// stages: 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열
 func solution(_ N: Int, _ stages: [Int]) -> [Int] {
     var challenger = stages.count
-    var stages = stages
-    stages.removeAll(where: { $0 == N + 1 })
-    stages.sort(by: { $0 < $1 })
+    let incompleters = Dictionary.init(grouping: stages) { $0 }
     
     return (1...N)
-        .map { stageNum -> (stage: Int, failureRate: Float) in
-            let firstIndex = stages.firstIndex(of: stageNum) ?? 0
-            let lastIndex = stages.lastIndex(of: stageNum) ?? -1
-            let incompleter = lastIndex - firstIndex + 1
-            
-            let result = (stageNum, (Float(incompleter) / Float(challenger)))
+        .map { stageNum -> (stage: Int, failureRate: Double) in
+            let incompleter = incompleters[stageNum]?.count ?? 0
+            let result = (stageNum, (Double(incompleter) / Double(challenger)))
             challenger -= incompleter
             return result
         }
